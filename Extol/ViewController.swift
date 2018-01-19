c01e4f0210e3028c4c42bd48b5d8adda0e3c4ab4
Levi Gemmell <levi.gemmell@veeva.com>
2018-01-18 17:20:08 -0800

    Initial Commit

---
+++ Extol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Extol
+//
+//  Created by Levi Gemmell on 1/18/18.
+//  Copyright © 2018 Bearsnsuch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
