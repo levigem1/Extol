eb909906f330f83ce34ab35e69657093f8749d3a
Levi Gemmell <levi.gemmell@veeva.com>
2018-01-27 00:37:01 -0800

    adding files

---
+++ Extol/Scenes/Artists/ArtistsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ArtistsViewController.swift
+//  Extol
+//
+//  Created by Levi Gemmell on 1/27/18.
+//  Copyright © 2018 Bearsnsuch. All rights reserved.
+//
+
+import Foundation
---
+++ Extol/Scenes/Seasons/SeasonsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SeasonsViewModel.swift
+//  Extol
+//
+//  Created by Levi Gemmell on 1/27/18.
+//  Copyright © 2018 Bearsnsuch. All rights reserved.
+//
+
+import Foundation
