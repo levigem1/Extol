938c31714ae38e3220e6b1c2d80914d04116c1c2
Levi Gemmell <levi.gemmell@veeva.com>
2018-01-26 22:21:36 -0800

    setting up tab bar

--- Extol/Scenes/HomePage/HomePageViewController.swift
+++ Extol/Scenes/HomePage/HomePageViewController.swift
@@ -2,12 +2,18 @@ import Foundation
 import UIKit
 
 class HomePageViewController: UIViewController {
+    let SEASONS = 0
+    let EXTOL = 1
+    let ARTISTS = 2
+    
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var spotifyButton: UIButton!
+    @IBOutlet weak var tabBar: UITabBar!
     
     override func viewDidLoad() {
+        tabBar.selectedItem = tabBar.items?[EXTOL]
         backgroundImage.image = #imageLiteral(resourceName: "Stars")
         titleLabel.text = "Extol"
         welcomeLabel.text = "Welcome to Extel, a place where worship, scripture, and devotion collide."
