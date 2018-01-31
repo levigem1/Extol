05a51120d37247e03c70caa01cdc16ffc13d49e4
Levi Gemmell <levi.gemmell@veeva.com>
2018-01-30 18:39:09 -0800

    remove sleep and update constraints

--- Extol/AppDelegate.swift
+++ Extol/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        sleep(1)
         // Override point for customization after application launch.
         return true
     }
